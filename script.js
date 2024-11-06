ef301639c574532ced6ce1d4f1d36e8fd62c0811
erina <ealejo@stanford.edu>
2024-11-05 20:57:13 -0800

    group work

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let userFlavors = prompt("Enter your order");
-// let userFlavors = []
+
+// enter:  vanilla,vanilla,vanilla,strawberry,coffee,coffee
 if (userFlavors) {
     // let flavorsArray = userFlavors.split(",")
 } 
@@ -7,54 +8,3 @@ console.log(userFlavors)
 userFlavor = [ "", ]
  
 console.table(userFlavors)
-
-// The user's input string is split into an array of strings.
-
-let flavorsArray = userFlavors.split(",")
-
-console.log(flavorsArray)
-// A loop is used to iterate through the array of flavors
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
-
-
-// An object is used to keep count of how many orders there are of each flavor.
-
-// The program correctly counts the number of each flavor in the user's input.
-
-// The logic for counting the frequencies of elements in an array is organized into a function that returns an object.
-
-// Names are consistent and meaningful.
-
-
-let userFlavors = prompt("Enter your order");
-
-if (userFlavors) {
-
-}
-console.log(userFlavors)
-userFlavor = ["",]
-
-console.table(userFlavors)
-
-// The user's input string is split into an array of strings.
-
-let flavorsArray = userFlavors.split(",")
-
-console.table(flavorsArray)
-// A loop is used to iterate through the array of flavors
-
-flavorsArray.forEach(element => {
-    
-});
-
-// An object is used to keep count of how many orders there are of each flavor.
-
-// The program correctly counts the number of each flavor in the user's input.
-
-
-// The logic for counting the frequencies of elements in an array is organized into a function that returns an object.
-
-// Names are consistent and meaningful.
