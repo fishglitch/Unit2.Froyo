0be45231a2ab340d18d336142d88a9793acfd187
erina <ealejo@stanford.edu>
2024-11-05 21:04:31 -0800

    group work

--- group.js
+++ group.js
@@ -1,35 +1,4 @@
 let userFlavors = prompt("Enter your order");
-// let userFlavors = []
-if (userFlavors) {
-    // let flavorsArray = userFlavors.split(",")
-} 
-console.log(userFlavors)
-userFlavor = [ "", ]
- 
-console.table(userFlavors)
-
-// The user's input string is split into an array of strings.
-
-let flavorsArray = userFlavors.split(",")
-
-console.log(flavorsArray)
-// A loop is used to iterate through the array of flavors
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
-
-
-// An object is used to keep count of how many orders there are of each flavor.
-
-// The program correctly counts the number of each flavor in the user's input.
-
-// The logic for counting the frequencies of elements in an array is organized into a function that returns an object.
-
-// Names are consistent and meaningful.
-
-
-let userFlavors = prompt("Enter your order");
 
 if (userFlavors) {
 
